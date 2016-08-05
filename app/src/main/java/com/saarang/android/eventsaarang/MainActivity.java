c00cc996c5bb1dcc9ebcdf51d086d85354d090d8
Abhishek <abhishekaroor@gmail.com>
2016-08-05 23:15:59 +0530

    Created 2 RecyclerViews. Made Layout for Main Page. (Error: app crashing)

--- app/src/main/java/com/saarang/android/eventsaarang/MainActivity.java
+++ app/src/main/java/com/saarang/android/eventsaarang/MainActivity.java
@@ -2,8 +2,15 @@ package com.saarang.android.eventsaarang;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +18,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<String> titleList  = Arrays.asList("Music Events", "Classical Events", "Word Games");
+        RecyclerView RV_category= (RecyclerView) findViewById(R.id.rv_categories);
+        RV_category.setLayoutManager(new LinearLayoutManager(this));
+        RV_category.setAdapter( new CategoryAdapter(titleList) );
+
+        RecyclerView RV_eventHorizontal = (RecyclerView) findViewById(R.id.rv_eventHorizontal);
+        RV_eventHorizontal.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false));
+        RV_eventHorizontal.setAdapter( new EventAdapter(titleList) );
+
 
         boolean firstRun = getSharedPreferences("PREFERENCE", MODE_PRIVATE).getBoolean("firstRun", true);
         //Add events to the database only once for the first time
